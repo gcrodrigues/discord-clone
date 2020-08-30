7b95c6c63786f5e57d37988934f96c4cbc2c4a4b
Gustavo <gcrodrigues413@gmail.com>
2020-08-30 17:32:38 -0300

    ChannelInfo component development

---
+++ src/components/ChannelInfo/styles.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { Hashtag } from "styled-icons/heroicons-outline";
+
+export const Container = styled.div`
+  grid-area: CI;
+
+  display: flex;
+  align-items: center;
+
+  padding: 0 17px;
+  background-color: var(--primary);
+  box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.2);
+  z-index: 2;
+`;
+
+export const HashtagIcon = styled(Hashtag)`
+  width: 24px;
+  height: 24px;
+  color: var(--symbol);
+`;
+
+export const Title = styled.p`
+  margin-left: 9px;
+
+  font-size: 16px;
+  font-weight: bold;
+
+  color: var(--white);
+`;
+
+export const Separator = styled.div`
+  height: 24px;
+  width: 1px;
+
+  background-color: var(--white);
+  opacity: 0.2;
+
+  margin: 0 13px;
+`;
+
+export const Description = styled.span`
+  font-size: 15px;
+  color: var(--gray);
+`;
